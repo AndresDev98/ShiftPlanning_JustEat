3d0edec72503af7088c01f5c75a7aeb54897faa1
Andres <desarrolladorapi@gmail.com>
2025-06-23 10:57:05 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ShiftPlanning.Application.Interfaces;
+using ShiftPlanning.Application.Services;
+using ShiftPlanning.Infrastructure.Services;
+using ShiftPlanning.Infrastructure;
+
+var services = new ServiceCollection();
+
+// Ruta donde tienes el directorio "tessdata"
+string tessdataPath = @"C:\Program Files\Tesseract-OCR\tessdata";
+
+// Registrar servicios
+services.AddSingleton<IShiftImageProcessor>(new TesseractShiftImageProcessor(tessdataPath));
+services.AddSingleton<IShiftMonitorService, ShiftMonitorService>();
+services.AddSingleton<ShiftPlanning.Infrastructure.Services.ShiftPlannerEngine>();
+
+var serviceProvider = services.BuildServiceProvider();
+
+// Ruta de prueba
+string prevImage = @"captures\\1\\imag_previa.png";
+string currImage = @"captures\\1\\img_despues.png";
+string outputFolder = @"captures\\1";
+
+var engine = serviceProvider.GetRequiredService<ShiftPlanning.Infrastructure.Services.ShiftPlannerEngine>();
+await engine.CompareAndLogAsync(prevImage, currImage, outputFolder);
---
+++ README.txt
@@ -0,0 +1,17 @@
+
+# Aplicación de consola.
+# .NET 8.0
+
+#Arquitectura 
+
+ShiftPlanning/
+├── ShiftPlanning.Domain/
+│   └── ShiftPlanning.Domain.csproj
+├── ShiftPlanning.Application/
+│   └── ShiftPlanning.Application.csproj
+├── ShiftPlanning.Infrastructure/
+│   └── ShiftPlanning.Infrastructure.csproj
+├── ShiftPlanning.Console/
+│   ├── Program.cs
+│   └── ShiftPlanning.Console.csproj
+└── ShiftPlanning.sln
